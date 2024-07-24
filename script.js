9d8f4a4e9a28d677df8bbeb07617800740954594
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-07-24 21:59:07 +0530

    removed unnecessary comments, only added comments that are important.

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
 
-
-/* logic/psudocode to get computer's choice.
-
-create a variable called options and store strings in array.
-creating a function with name getComputerChoice.
-create a variable called randomOptions
-assign the arrey to the variable randomOptions.
-the array should contain a math.random multiplied by array length and we should Math.floor the value.
-console.log the randomOptions.
-output that variable.
-*/
-
 let options = ['rock', 'paper', 'scissors'];
 
 function getComputerOptions(){
  let randomOptions = options[(Math.floor(Math.random()*options.length))];
  return randomOptions;
 }
-
-/*
-
-create a function called getHumanChoice
-create a variable called choice and assign a prompt asking user for their choice.
-create a conditional if statement to make sure other options entered by user are invalid.
-output the choices in each condition.
-*/
 
 function getHumanChoice(){
     choice = prompt("choose any one: rock, paper, scissors");
@@ -47,34 +27,12 @@ function getHumanChoice(){
     }   
 }
 
-/* creating variable to keep track of scores and initializing it with zero*/
 let humanScore = 0;
 let computerScore = 0;
-
-/*
-creating a function playgame
-move playRound function and score variables inside the playGame function
-there should be 5 rounds.
-create a for loop for variable round, if round is equal to one
-round is less than 6, increatement round.
-*/
 
 function playGame(){
 
     for(let round = 1; round<6; round++){
-
-
-/*
- writing a logic to play a single round.
- create a function playRound.
- it takes two parameters: humanChoice and computerChoice
- create two variables in global scope called humanSelection and comSection and assign the getHumanChoice function and getComputerChoice function respectively.
- change human choices and computerChoices to case insensitive by using toLowerCase()
- in playRound function create a conditional statement between humanchoices and computerchoices.
- if both are strictly equal then its a draw
- if they are not equal then write condition explaing game logic
- increment the human or com variable based on the winner.
-*/
 
 function playRound(humanChoice,computerChoice){
 humanChoice = humanChoice.toLowerCase();
@@ -115,9 +73,10 @@ if(humanScore>computerScore){
 else if(computerScore>humanScore){
     console.log('Gameover!!! better luck next time! :(')
 }
+
+// the following scores are working only when it is placed outside the loop.
 console.log(`your score: ${humanScore}`);
 console.log(`computer score: ${computerScore}`);
-
 
 }
 
